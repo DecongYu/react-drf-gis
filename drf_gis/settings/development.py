b6a694bd6286a552126a4c3d9e52370f8e69b7b5
Decong Yu <decong.yu@gmail.com>
2022-06-06 09:22:21 -0600

    update settings

---
+++ drf_gis/settings/development.py
@@ -0,0 +1,12 @@
+from .base import *
+
+DATABASE = {
+    'default': {
+        'ENGINE': env('POSTGRES_ENGINE'),
+        'NAME': env('POSTGRES_DB'),
+        'USER': env('POSTGRES_USER'),
+        'PASSWORD': env('POSTGRES_PASSWORD'),
+        'HOST': env('POSTGRES_HOST'),
+        'PORT': env('POSTGRES_PORT'),
+    }
+}
