174c406760bcf542b7a672b05ac27859e6362800
Decong Yu <decong.yu@gmail.com>
2022-06-09 14:15:40 -0600

    add profiles

--- drf_gis/settings/development.py
+++ drf_gis/settings/development.py
@@ -1,6 +1,8 @@
 from .base import *
 
-
+# To create a postgres or postgis database for Django backends
+# https://hatarilabs.com/ih-en/how-to-install-postgresql-and-postgis-in-windows-10-with-wsl-and-debian-tutorial
+# https://docs.microsoft.com/en-us/windows/wsl/tutorials/wsl-database
 DATABASES = {
     'default': {
         'ENGINE': env('POSTGRES_ENGINE'),
